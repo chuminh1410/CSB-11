12ad7f41eaf7f608b858e95910ad6f5c8361363d
Chu Minh <chungocquangminh@gmail.com>
2023-05-15 00:42:36 +0700

    missing error handling for inputting text rather than number

--- Hackathon/Hackathon 1/Part 5/final.py
+++ Hackathon/Hackathon 1/Part 5/final.py
@@ -17,23 +17,26 @@ def generate_question():
     return question, correct_answer, eval(incorrect_answer)
 
 
-print("== FREAKING MATH CONSOLE ==")
-print("Give correct answers to get scores.")
-score = 0
-game_over = False
-while not game_over:
-    question, correct_answer, incorrect_answer  = generate_question()
-    random_ans = random.choice([correct_answer,incorrect_answer])
-    print(question, "=", random_ans )
-    answer = int(input("1 for True, 0 for False: "))
-    if answer == 1 and random_ans == correct_answer :
-        score += 1
-        print("Score:", score)
-    elif answer == 0 and random_ans != correct_answer:
-        score += 1
-        print("Score:", score)
-    else:
-        print("Incorrect.")
-        game_over = True
-print("== GAME OVER ==")
-print("Your total score is", score)
+def main_game():
+    print("== FREAKING MATH CONSOLE ==")
+    print("Give correct answers to get scores.")
+    score = 0
+    game_over = False
+    while not game_over:
+        question, correct_answer, incorrect_answer  = generate_question()
+        random_ans = random.choice([correct_answer,incorrect_answer])
+        print(question, "=", random_ans )
+        answer = int(input("1 for True, 0 for False: "))
+        if answer == 1 and random_ans == correct_answer :
+            score += 1
+            print("Score:", score)
+        elif answer == 0 and random_ans != correct_answer:
+            score += 1
+            print("Score:", score)
+        else:
+            print("Incorrect.")
+            game_over = True
+    print("== GAME OVER ==")
+    print("Your total score is", score)
+
+main_game()
