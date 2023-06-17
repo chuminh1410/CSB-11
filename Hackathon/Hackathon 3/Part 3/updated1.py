61bba1be2a686fb717e2145caaadacf0867bf4e6
chuminh1410 <chungocquangminh@gmail.com>
2023-06-17 12:30:45 +0700

    123

--- Hackathon/Hackathon 3/Part 3/updated1.py
+++ Hackathon/Hackathon 3/Part 3/updated1.py
@@ -5,7 +5,11 @@ import random
 MAP_SIZE = int(input("Enter the map size you want: "))
 
 PLAYER = 'P'
-KEY = 'K'
+KEYS = [
+    {'symbol': '1', 'position': [random.choice(range(MAP_SIZE)), random.choice(range(MAP_SIZE))], 'order': 1},
+    {'symbol': '2', 'position': [random.choice(range(MAP_SIZE)), random.choice(range(MAP_SIZE))], 'order': 2},
+    {'symbol': '3', 'position': [random.choice(range(MAP_SIZE)), random.choice(range(MAP_SIZE))], 'order': 3},
+]
 DOOR = 'D'
 EMPTY = '-'
 
@@ -22,52 +26,65 @@ def initialize_map():
     game_map = [[EMPTY] * MAP_SIZE for _ in range(MAP_SIZE)]
     game_map[player_position[0]][player_position[1]] = PLAYER
 
-    while True:
-        key_row = random.randint(0, MAP_SIZE - 1)
-        key_col = random.randint(0, MAP_SIZE - 1)
-        if key_row != player_position[0] or key_col != player_position[1]:
-            break
+    for key in KEYS:
+        while True:
+            key_row = key['position'][0]
+            key_col = key['position'][1]
+            if (key_row != player_position[0] or key_col != player_position[1]) and game_map[key_row][key_col] == EMPTY:
+                break
+
+        game_map[key_row][key_col] = key['symbol']
+        key['collected'] = False
 
     while True:
         door_row = random.randint(0, MAP_SIZE - 1)
         door_col = random.randint(0, MAP_SIZE - 1)
-        if (door_row != player_position[0] or door_col != player_position[1]) and (door_row != key_row or door_col != key_col):
+        if (door_row != player_position[0] or door_col != player_position[1]) and game_map[door_row][door_col] == EMPTY:
             break
 
-    game_map[key_row][key_col] = KEY
     game_map[door_row][door_col] = DOOR
 
     return game_map
 
-
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
-
 
 def draw_map(game_map):
     clear_screen()
     print("=== THE ESCAPE GAME ===")
-    print("Find (K)ey first and get to the (D)oor")
+    print("Find Keys in order (1,2,3) first and get to the (D)oor")
     print()
 
     for row in game_map:
         print(' '.join(row))
     print()
 
+    for key in KEYS:
+        if not key['collected']:
+            print(f"Collect {key['symbol']} (Order: {key['order']})")
+
+    print(message)
+
+    if all(key['collected'] for key in KEYS if key['order'] == 1) and all(key['collected'] for key in KEYS if key['order'] == 2) and all(key['collected'] for key in KEYS if key['order'] == 3):
+        playing = False
+        draw_game_won()
 
 def draw_game_over():
+    global playing
     clear_screen()
     print("=== GAME OVER ===")
     print("You failed to escape!")
     print()
-
+    playing = False
 
 def draw_game_won():
+    global playing, message
     clear_screen()
     print("=== CONGRATULATIONS! ===")
     print("You have successfully escaped!")
     print()
-
+    playing = False
+    message = ""
 
 def move_player(move, game_map):
     global playing, key_collected, message, player_position
@@ -84,21 +101,38 @@ def move_player(move, game_map):
         return
 
     if 0 <= next_position[0] < MAP_SIZE and 0 <= next_position[1] < MAP_SIZE:
-        if game_map[next_position[0]][next_position[1]] == KEY:
-            game_map[next_position[0]][next_position[1]] = EMPTY
-            key_collected = True
-            game_map[player_position[0]][player_position[1]] = EMPTY
-            player_position = next_position
-            game_map[player_position[0]][player_position[1]] = PLAYER
-            message = "You have acquired the key!"
-        elif game_map[next_position[0]][next_position[1]] == DOOR:
-            if door_position[0] == player_position[0] and door_position[1] == player_position[1] and key_collected:
+        next_cell = game_map[next_position[0]][next_position[1]]
+
+        if next_cell in [key['symbol'] for key in KEYS]:
+            key_symbol = next_cell
+            key_order = next(key['order'] for key in KEYS if key['symbol'] == key_symbol)
+
+            if key_order == 1 or (key_order - 1 in [collected_key['order'] for collected_key in KEYS if collected_key['collected']]):
+                game_map[next_position[0]][next_position[1]] = EMPTY
+                key_collected = True
+
+                for key in KEYS:
+                    if key['symbol'] == key_symbol:
+                        key['collected'] = True
+                        break
+
+                game_map[player_position[0]][player_position[1]] = EMPTY
+                player_position = next_position
+                game_map[player_position[0]][player_position[1]] = PLAYER
+                message = f"You have acquired key {key_symbol}!"
+
+                if all(key['collected'] for key in KEYS if key['order'] == 1) and all(key['collected'] for key in KEYS if key['order'] == 2) and all(key['collected'] for key in KEYS if key['order'] == 3):
+                    playing = False
+                    draw_game_won()
+
+        elif next_cell == DOOR:
+            if all(key['collected'] for key in KEYS):
                 playing = False
                 draw_game_won()
-
             else:
-                message = "You need to find the key first"
-        elif game_map[next_position[0]][next_position[1]] == EMPTY:
+                message = "You need to collect all keys first"
+
+        elif next_cell == EMPTY:
             game_map[player_position[0]][player_position[1]] = EMPTY
             player_position = next_position
             game_map[player_position[0]][player_position[1]] = PLAYER
@@ -108,21 +142,12 @@ def move_player(move, game_map):
     if not playing:
         return
 
-    if door_position == player_position and key_collected:
-        playing = False
-        draw_game_won()
-
-
-
-
-
 game_map = initialize_map()
 
 clear_screen()
 print("=== THE ESCAPE GAME ===")
 print("Use W/A/S/D to move (P)layer, press Q to quit")
 print("Find (K)ey first and get to the (D)oor")
-
 
 while playing:
     draw_map(game_map)
